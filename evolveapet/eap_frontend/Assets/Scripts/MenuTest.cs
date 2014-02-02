180655dfba6e4560d126e854a17751b5a5f1e8e9
Marianne Gosciniak <gomath@mit.edu>
2014-02-02 14:59:00 +0000

    Added Main Menu, which currently does nothing. Will center and polish appearance later.

---
+++ evolveapet/eap_frontend/Assets/Scripts/MenuTest.cs
@@ -0,0 +1,29 @@
+﻿//C#
+using UnityEngine;
+using System.Collections;
+
+public class MenuTest : MonoBehaviour {
+	
+	void OnGUI () {
+		// Make a background box
+		GUI.Box(new Rect(10,10,320,130), "Main Menu");
+		
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(20,40,300,20), "New Game")) {
+			//will presumably call some function to change the scene to that of the main game
+			Application.LoadLevel(1);
+		}
+		
+		// Make the second button.
+		if(GUI.Button(new Rect(20,70,300,20), "Load Saved Game")) {
+			//will presumably call some function to change the scene to a load menu
+			Application.LoadLevel(2);
+		}
+
+		// Make the third button.
+		if(GUI.Button(new Rect(20,100,300,20), "Quit")) {
+			//quit
+			Application.Quit();
+		}
+	}
+}
