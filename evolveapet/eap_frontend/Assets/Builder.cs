f4ca82a3599eb1c0ae310bd0087f8e0387ad24d8
TomLefley <thomas.lefley@gmail.com>
2014-02-11 16:24:47 +0000

    Can now create random animals
    Pressing 'play' in the scene 'Animal' will generate a random animal

---
+++ evolveapet/eap_frontend/Assets/Builder.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EvolveAPet {
+	public class Builder : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			StartCoroutine("Build");
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		IEnumerator Build() {
+			//Remove the previous animal
+			GameObject.Destroy(GameObject.FindGameObjectWithTag("Animal"));
+			Resources.UnloadUnusedAssets();
+			
+			//Wait one frame for destroys to commit
+			yield return new WaitForSeconds(0f);
+			
+			//Create new animal
+			GameObject animal = (GameObject)Instantiate(Resources.Load ("Prefabs/animal"));
+			animal.GetComponent<Animal>().Build();
+		}
+	}
+}
--- evolveapet/eap_frontend/Assets/Scripts/Backend/Animal.cs
+++ evolveapet/eap_frontend/Assets/Scripts/Backend/Animal.cs
@@ -37,7 +37,8 @@ namespace EvolveAPet
         }
 
 		public Animal(){// Generates a completely random animal
-			
+
+			BodyPartArray = new BodyPart[bodyPartNumber]; 
 			Chromosome[] chromosomeArrayA = new Chromosome[bodyPartNumber];
 			Chromosome[] chromosomeArrayB = new Chromosome[bodyPartNumber];
 			int genePos;
@@ -258,6 +259,127 @@ namespace EvolveAPet
 				GetComponent<Animator>().SetTrigger("Blink");
 				yield return new WaitForSeconds(UnityEngine.Random.Range (2f, 5f));
 			}	
+		}
+
+		public void Build() {
+			
+			//Find transforms
+			Transform tBody = GameObject.FindGameObjectWithTag("Body").transform;
+			Transform tHead = GameObject.FindGameObjectWithTag("Head").transform;
+			Transform tTail = GameObject.FindGameObjectWithTag("Tail").transform;
+			Transform tFrontLeg = GameObject.FindGameObjectWithTag("Front Leg").transform;
+			Transform tBackLeg = GameObject.FindGameObjectWithTag("Back Leg").transform;
+			Transform tFrontArm = GameObject.FindGameObjectWithTag("Front Arm").transform;
+			Transform tBackArm = GameObject.FindGameObjectWithTag("Back Arm").transform;
+			Transform[] tEyes = {GameObject.FindGameObjectWithTag("Eye 0").transform, GameObject.FindGameObjectWithTag("Eye 1").transform, GameObject.FindGameObjectWithTag("Eye 2").transform};
+			Transform tFrontEar = GameObject.FindGameObjectWithTag("Front Ear").transform;
+			Transform tBackEar = GameObject.FindGameObjectWithTag("Back Ear").transform;
+			
+			//Build body, rotate if bipedal and scale/colour as needed
+			Torso t = (Torso)BodyPartArray[3];
+			GameObject body = (GameObject)Instantiate(Resources.Load ("Prefabs/"+t.shape+" body"));
+			body.GetComponent<SpriteRenderer>().color = DecodeCol(t.colour);
+			if (!((Arms)BodyPartArray[4]).isQuadrupedal) {
+				body.transform.Rotate(new Vector3(0,0,-30));
+			}
+			body.transform.parent = tBody;
+			Scale(tBody, t.size);
+			
+			//build head, color eyes, choose mouth and scale/colour as needed
+			Head h = (Head)BodyPartArray[2];
+			GameObject head = (GameObject)Instantiate(Resources.Load ("Prefabs/"+h.shape+" head"));
+			head.GetComponent<SpriteRenderer>().color = DecodeCol (h.colour);
+			head.transform.position = tHead.position = GameObject.Find ("head joint").transform.position;
+			head.transform.parent = tHead;
+
+			//build eyes
+			Eyes e = (Eyes)BodyPartArray[1];
+			for (int i = 0; i<e.number; i++) {
+				GameObject eye = (GameObject)Instantiate (Resources.Load ("Prefabs/DINO eye"));
+				eye.transform.position = tEyes[i].position = GameObject.Find("eye joint "+i).transform.position;
+				eye.transform.parent = tEyes[i];
+				Scale (tEyes[i], e.size);
+				eye.GetComponent<SpriteRenderer>().color = DecodeCol (e.colour);
+			}
+
+			//build ears
+			/*Ears ea = (Ears)BodyPartArray[0];
+			GameObject ear = (GameObject)Instantiate(Resources.Load ("Prefabs/"+ea.shape+" front ear"));
+			ear.GetComponent<SpriteRenderer>().color = DecodeCol (ea.colour);
+			ear.transform.position = tFrontEar.position = GameObject.Find ("ear joint").transform.position;
+			ear.transform.parent = tFrontEar;
+			Scale(tFrontEar, ea.size);
+
+			ear = (GameObject)Instantiate(Resources.Load ("Prefabs/"+ea.shape+" back ear"));
+			ear.GetComponent<SpriteRenderer>().color = DecodeCol (ea.colour);
+			ear.transform.position = tBackEar.position = GameObject.Find ("ear joint").transform.position;
+			ear.transform.parent = tBackEar;
+			Scale(tBackEar, ea.size);*/
+			
+			Scale(tHead, h.size);
+
+			//build mouth
+			GameObject.Find ("mouth").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/"+h.shape+" "+h.teethShape+" mouth");
+			
+			//build tail, scale/colour as needed
+			Tail ta = (Tail)BodyPartArray[6];
+			GameObject tail = (GameObject)Instantiate(Resources.Load ("Prefabs/"+ta.shape+" tail"));
+			tail.GetComponent<SpriteRenderer>().color = DecodeCol (ta.colour);
+			tail.transform.position = tTail.position = GameObject.Find ("tail joint").transform.position;
+			tail.transform.parent = tTail;
+			Scale(tTail, ta.size);
+			
+			//build legs, scale/colour as needed
+			Legs l = (Legs)BodyPartArray[5];
+			GameObject leg = (GameObject)Instantiate(Resources.Load ("Prefabs/"+l.shape+" front leg"));
+			leg.GetComponent<SpriteRenderer>().color = DecodeCol(l.colour);
+			leg.transform.position = tFrontLeg.position = GameObject.Find ("leg joint").transform.position;
+			leg.transform.parent = tFrontLeg;
+			Scale(tFrontLeg, l.size);
+			
+			leg = (GameObject)Instantiate(Resources.Load ("Prefabs/"+l.shape+" back leg"));
+			leg.GetComponent<SpriteRenderer>().color = DecodeCol(l.colour);
+			leg.transform.position = tBackLeg.position = GameObject.Find ("leg joint").transform.position;
+			leg.transform.parent = tBackLeg;
+			Scale(tBackLeg, l.size);
+
+			//build arms, replace with legs if bipedal and scale/colour as needed
+			Arms a = (Arms)BodyPartArray[4];
+			string limb = a.isQuadrupedal ? "leg" : "arm";
+			
+			if (a.number != 0) {
+				GameObject arm = (GameObject)Instantiate(Resources.Load ("Prefabs/"+a.shape+" front "+limb));
+				arm.GetComponent<SpriteRenderer>().color = DecodeCol(a.colour);
+				arm.transform.position = tFrontArm.position = GameObject.Find ("arm joint").transform.position;
+				arm.transform.parent = tFrontArm;
+				if (a.isQuadrupedal) {
+					Scale(tFrontArm, l.size);
+				} else {
+					Scale(tFrontArm, a.size);
+				}
+				
+				arm = (GameObject)Instantiate(Resources.Load ("Prefabs/"+a.shape+" front "+limb));
+				arm.GetComponent<SpriteRenderer>().color = DecodeCol (a.colour);
+				arm.transform.position = tBackArm.position = GameObject.Find ("arm joint").transform.position;
+				arm.transform.parent = tBackArm;
+				if (a.isQuadrupedal) {
+					Scale(tBackArm, l.size);
+				} else {
+					Scale(tBackArm, a.size);
+				}
+			}
+			
+		}
+
+		float[] sizes = {0.9f, 1.0f, 1.1f};
+		
+		//Parse the scale ints and scale accordingly
+		void Scale (Transform t, int size) {
+			t.localScale = new Vector3(sizes[size],sizes[size], 1);
+		}
+
+		Color DecodeCol (int[] c) {
+			return new Color(c[0]/255f, c[1]/255f, c[2]/255f);
 		}
     }
 }
