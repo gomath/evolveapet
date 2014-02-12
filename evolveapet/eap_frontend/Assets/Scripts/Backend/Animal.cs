f20acae3bc63c800578924eb63857c29c725ed7c
Matej Hamas <matej.hamas@gmail.com>
2014-02-12 19:45:35 +0000

    Random breeding implemented - untested
    BreedMeRandomly(Animal a) function in Animal class
    - given another animal, this function returns the random offspring from
    these two parents.

--- evolveapet/eap_frontend/Assets/Scripts/Backend/Animal.cs
+++ evolveapet/eap_frontend/Assets/Scripts/Backend/Animal.cs
@@ -20,20 +20,27 @@ namespace EvolveAPet
         public bool Egg { get; set; }
         private readonly int bodyPartNumber = Global.NUM_OF_CHROMOSOMES; // to allow easy changing of number of body parts
 
+		/// <summary>
+		/// Create a new animal from 2 set of genes and 2 parents.
+		/// </summary> 
+		/// <param name="chromA">Chrom a.</param>
+		/// <param name="chromB">Chrom b.</param>
+		/// <param name="parent1">Parent1.</param>
+		/// <param name="parent2">Parent2.</param>
 		public Animal(Chromosome[] chromA, Chromosome[] chromB, Animal parent1, Animal parent2)
         {
-            Egg = true; //Animals will alwyas be in egg form when created
+            //Egg = true; //Animals will alwyas be in egg form when created
             //Firstly, populate all the genes in the genome
             Genome = new Genome(chromA, chromB);
             _parents = new Animal[] { parent1, parent2 };
-            BodyPartArray = new BodyPart[bodyPartNumber]; 
-            for (int n = 0; n < bodyPartNumber; n++)
-            {
-           //BodyPartArray[n] = createBodyPart((EnumBodyPart)n);
-            }
-			/*if (BodyPartArray[4].Number ==2 && BodyPartArray[5].Number==4){ // if the creature has 2 arms and 4 legs
-				throw new TooManyLimbsException("Quadrupedal animal with arms being created");
-			}*/
+
+            // New animal has max. generations of your parents + 1
+			Generation = (parent1.Generation > parent2.Generation) ? parent1.Generation+1 : parent2.Generation+1;
+
+			BodyPartArray = new BodyPart[bodyPartNumber]; 
+            for (int n = 0; n < bodyPartNumber; n++){
+				createBodyPart(n);
+			}
         }
 
 		public Animal(){// Generates a completely random animal
@@ -41,6 +48,7 @@ namespace EvolveAPet
 			BodyPartArray = new BodyPart[bodyPartNumber]; 
 			Chromosome[] chromosomeArrayA = new Chromosome[bodyPartNumber];
 			Chromosome[] chromosomeArrayB = new Chromosome[bodyPartNumber];
+			Generation = 1;
 			int genePos;
 			Gene newGene; // mother and father genes
 			/*6 traits filled in this order :
@@ -65,9 +73,6 @@ namespace EvolveAPet
 			for (int n = 0; n<bodyPartNumber;n++){// iterate through body parts
 				Chromosome currentChromosomeA = new Chromosome (n); // creates a new chromosome for each body part
 				Chromosome currentChromosomeB = new Chromosome (n); // creates a new chromosome for each body part
-
-
-
 
 				for (int t=0;t<6;t++){ // iterate through all traits
 
@@ -210,7 +215,32 @@ namespace EvolveAPet
 				BodyPartArray[6] = new Tail(rgbArray, sizeNum, shapeStr, patternNumber);
 				break;
 			}
-
         }
+
+		/// <summary>
+		/// Given an animal, randomly breeds this animal with that given and returns offspring. 
+		/// </summary>
+		/// <returns>The me randomly.</returns>
+		/// <param name="a">The alpha component.</param>
+		public Animal BreedMeRandomly(Animal a){
+			Chromosome[,] tetrads1 = Genome.CreateTetradsForBreeding ();
+			Chromosome[,] tetrads2 = a.Genome.CreateTetradsForBreeding ();
+
+			Chromosome[] parent1 = new Chromosome[Global.NUM_OF_CHROMOSOMES];
+			Chromosome[] parent2 = new Chromosome[Global.NUM_OF_CHROMOSOMES];
+
+			for (int i=0; i<Global.NUM_OF_CHROMOSOMES; i++) {
+				int j = Global.rand .Next(4);
+				parent1[i] = tetrads1[i,j];
+
+				j = Global.rand.Next(4);
+				parent2[i] = tetrads2[i,j];
+			}
+
+			Animal offspring = new Animal(parent1, parent2, this, a);
+			return offspring;
+		}
+	
 	}
+	
 }
