21c7f88cf288a26402cdc9453af9fa751b611ada
Matej Hamas <matej.hamas@gmail.com>
2014-02-13 14:45:21 +0000

    Main menu minor change - Marianne

--- evolveapet/eap_frontend/Assets/Scripts/Frontend/MainMenu.cs
+++ evolveapet/eap_frontend/Assets/Scripts/Frontend/MainMenu.cs
@@ -8,19 +8,19 @@ public class MainMenu : MonoBehaviour {
 		// Make a background box
 		
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(525,50,75,20), "New Game")) {
+		if(GUI.Button(new Rect(475,50,75,20), "New Game")) {
 			//for now loads Animal Scene; should load stable scene
-			Application.LoadLevel("Animal");
+			Application.LoadLevel("RandomBreeding");
 		}
 		
 		// Make the second button.
-		if(GUI.Button(new Rect(450,150,120,20), "Load Saved Game")) {
+		if(GUI.Button(new Rect(400,150,120,20), "Load Saved Game")) {
 			//will presumably call some function to change the scene to a load menu
 			Application.LoadLevel("test suite");
 		}
 
 		// Make the third button.
-		if(GUI.Button(new Rect(440,260,50,20), "Quit")) {
+		if(GUI.Button(new Rect(390,260,50,20), "Quit")) {
 			//quit
 			Application.Quit();
 		}
