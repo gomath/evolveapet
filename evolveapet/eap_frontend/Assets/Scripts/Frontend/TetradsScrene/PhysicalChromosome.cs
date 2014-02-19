146d1af202e23f231c187625b95a9a41bf954eb5
Matej Hamas <matej.hamas@gmail.com>
2014-02-19 23:20:09 +0000

    Create Tetrads Screne Basic Functionality Added
    Scrolling is possible, as well as you can click on chromosomes and change colours of centromeres. After clicking, they are associated with one of the boxes on the right and you can navigate to the correct tetrads by simply clicking on that box.
    
    TODO - chromosomes miniature should appear in black box
    
    TODO 2 - magnifying class showing chromosome details

---
+++ evolveapet/eap_frontend/Assets/Scripts/Frontend/TetradsScrene/PhysicalChromosome.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EvolveAPet{
+
+public class PhysicalChromosome : MonoBehaviour {
+	
+	private Chromosome chromosome;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		SendMessageUpwards("ChromosomeClicked",this);
+
+	}
+
+	public void PutMyTetradIntoCentre(){
+		SendMessageUpwards ("TranslateAllTetrads");
+	}
+
+
+}
+}
---
+++ evolveapet/eap_frontend/Assets/Scripts/Frontend/TetradsScrene/TetradBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace EvolveAPet{
+
+public class TetradBehaviour : MonoBehaviour {
+
+	private GameObject activeChromosome;
+	private GameObject myBox;
+
+	// Use this for initialization
+	void Start () {
+		short boxNum = Convert.ToInt16(name.Substring(name.Length-1,1));
+		myBox = GameObject.Find ("Box" + boxNum);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ChromosomeClicked(PhysicalChromosome chromosome){
+
+		GameObject centromere = chromosome.transform.FindChild("centromere").FindChild("centromere colour").gameObject;
+
+		if (centromere.GetComponent<SpriteRenderer> ().color != Color.green) {
+			centromere.GetComponent<SpriteRenderer> ().color = Color.green;
+		} else {
+			centromere.GetComponent<SpriteRenderer> ().color = Color.red;
+		}
+
+		myBox.GetComponent<BoxBehaviour>().CreateChromosomeMirror(chromosome);
+	}
+
+	void TranslateAllTetrads(){
+		float offset = transform.localPosition.y;
+		SendMessageUpwards ("TranslateAllTetradsByOffset", offset);
+	}
+}
+}
