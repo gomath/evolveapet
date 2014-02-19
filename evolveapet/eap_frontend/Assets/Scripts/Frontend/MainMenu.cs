4ab6ebb9738517a94002aecab502176a15198de3
Marianne Gosciniak <gomath@mit.edu>
2014-02-19 23:20:27 +0000

    Menu buttons now align correctly if 16:9 ratio regardless of resolution
    Looks less than ideal (stretching etc.) for other aspect ratios but the approximate locations are acceptable. Thanks Tom for the GUI.matrix tip.

--- evolveapet/eap_frontend/Assets/Scripts/Frontend/MainMenu.cs
+++ evolveapet/eap_frontend/Assets/Scripts/Frontend/MainMenu.cs
@@ -3,26 +3,38 @@ using UnityEngine;
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
-	
+	float originalWidth = 1098.0f;
+	float originalHeight = 618.0f;
+	Vector3 scale = new Vector3();
+
 	void OnGUI () {
-		// Make a background box
+		scale.x = Screen.width / originalWidth;
+		scale.y = Screen.height / originalHeight;
+		scale.z = 1;
+		var svMat = GUI.matrix;
+
+		// substitute matrix to scale if screen nonstandard
+		GUI.matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, scale);
 		
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(475,50,75,20), "New Game")) {
+		if(GUI.Button(new Rect(700,82,150,40), "New Game")) {
 			//for now loads Animal Scene; should load stable scene
 			Application.LoadLevel("RandomBreeding");
 		}
 		
 		// Make the second button.
-		if(GUI.Button(new Rect(400,150,120,20), "Load Saved Game")) {
+		if(GUI.Button(new Rect(640,272,120,40), "Load Saved Game")) {
 			//will presumably call some function to change the scene to a load menu
 			Application.LoadLevel("test suite");
 		}
 
 		// Make the third button.
-		if(GUI.Button(new Rect(390,260,50,20), "Quit")) {
+		if(GUI.Button(new Rect(580,470,50,40), "Quit")) {
 			//quit
 			Application.Quit();
 		}
+
+		//restore matrix before return
+		GUI.matrix = svMat;
 	}
 }
