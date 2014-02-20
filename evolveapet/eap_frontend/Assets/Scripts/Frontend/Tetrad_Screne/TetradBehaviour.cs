93d0809410095effa6edd87f34e32ae378b85370
Matej Hamas <matej.hamas@gmail.com>
2014-02-20 23:28:31 +0000

    Changes to create tetrads screen
    Note that chromosomes are at moment created only at random and in backend representation.

---
+++ evolveapet/eap_frontend/Assets/Scripts/Frontend/Tetrad_Screne/TetradBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace EvolveAPet{
+
+public class TetradBehaviour : MonoBehaviour {
+
+	private PhysicalChromosome activeChromosome;
+		private int tetradNum;
+		private int chromosomeNum;
+	private GameObject myBox;
+
+	// Use this for initialization
+	void Start () {
+		tetradNum = Convert.ToInt32(name.Substring(name.Length-1,1));
+		myBox = GameObject.Find ("Box" + tetradNum);
+		
+		// Setting chromosome numbers
+			transform.FindChild ("chromosome pair " + tetradNum + "A").FindChild ("chromosome m").gameObject.GetComponent<PhysicalChromosome> ().chromosomeNumber = 0;
+			transform.FindChild ("chromosome pair " + tetradNum + "A").FindChild ("chromosome f").gameObject.GetComponent<PhysicalChromosome> ().chromosomeNumber = 1;
+			transform.FindChild ("chromosome pair " + tetradNum + "B").FindChild ("chromosome m").gameObject.GetComponent<PhysicalChromosome> ().chromosomeNumber = 2;
+			transform.FindChild ("chromosome pair " + tetradNum + "B").FindChild ("chromosome f").gameObject.GetComponent<PhysicalChromosome> ().chromosomeNumber = 3;
+		// Randomly select one chromosome at the beginning
+		string AB = "A";
+		string mf = "m";
+		int r = Global.rand.Next (4);
+			switch(r){
+			case 0: AB = "A"; mf = "m"; chromosomeNum = 0; break; 
+			case 1: AB = "A"; mf = "f"; chromosomeNum = 1; break;
+			case 2: AB = "B"; mf = "m"; chromosomeNum = 2; break;
+			case 3: AB = "B"; mf = "f"; chromosomeNum = 3; break;
+			}
+
+		activeChromosome = transform.FindChild ("chromosome pair " + tetradNum + AB).FindChild("chromosome " + mf).gameObject.GetComponent<PhysicalChromosome>();
+		ChromosomeClicked (activeChromosome);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ChromosomeClicked(PhysicalChromosome chromosome){
+
+		if (activeChromosome != null) {
+			// Setting old centromere colour to white
+			activeChromosome.transform.FindChild("centromere").FindChild("centromere colour").gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+		}
+		// Setting new centrome colour to green
+		activeChromosome = chromosome;
+		activeChromosome.transform.FindChild ("centromere").FindChild ("centromere colour").gameObject.GetComponent<SpriteRenderer> ().color = Color.green;
+		myBox.GetComponent<BoxBehaviour>().CreateChromosomeMirror(chromosome);
+		myBox.GetComponent<BoxBehaviour> ().SetGenderSign (activeChromosome.chromosomeNumber);
+	}
+
+	void TranslateAllTetrads(){
+		float offset = transform.localPosition.y;
+		SendMessageUpwards ("TranslateAllTetradsByOffset", offset);
+	}
+}
+}
---
+++ evolveapet/eap_frontend/Assets/Scripts/Frontend/Tetrad_Screne/TetradsScroll.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EvolveAPet {
+
+public class TetradsScroll : MonoBehaviour {
+
+	GameObject[] tetrads;
+	public float scrollingSensitivity = 300;
+	public float scrollForce = 0;
+	public float dumpingFactor = 2;
+	public float scrollingTreshold = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		tetrads = new GameObject[Global.NUM_OF_CHROMOSOMES];
+		for (int i=0; i<Global.NUM_OF_CHROMOSOMES; i++) {
+			tetrads[i] = GameObject.Find("Tetrad_" + i);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float actualMaxY = tetrads [0].transform.localPosition.y;
+		float actualMinY = tetrads [Global.NUM_OF_CHROMOSOMES-1].transform.localPosition.y; 
+		
+			float scroll = -Input.GetAxis ("Mouse ScrollWheel");
+			if((scroll > scrollingTreshold  && actualMinY > -1) ||
+			   (scroll < -scrollingTreshold && actualMaxY < 1) 
+			   || (scrollForce > scrollingTreshold && actualMinY > -1) 
+			   || (scrollForce < -scrollingTreshold && actualMaxY < 1)) {
+				if((scrollForce > scrollingTreshold && actualMinY > -1) ||(scrollForce < scrollingTreshold && actualMaxY < 1)){
+					scrollForce = 0;
+				}
+				return;
+			}
+
+			if (Mathf.Abs (scroll) > scrollingTreshold) {
+				scrollForce += scroll;
+				foreach (GameObject tetrad in tetrads) {
+					tetrad.transform.Translate (new Vector2(0,scroll * Time.deltaTime * scrollingSensitivity));
+				}
+			} else if(Mathf.Abs(scrollForce) > scrollingTreshold) {
+				foreach (GameObject tetrad in tetrads) {
+					tetrad.transform.Translate (new Vector2(0,scrollForce * Time.deltaTime * scrollingSensitivity));
+				}
+
+				// Dumping scrolling
+				scrollForce /= dumpingFactor;
+			}
+		
+	}
+
+	public void TranslateAllTetradsByOffset(float offset){
+		foreach (GameObject tetrad in tetrads) {
+			tetrad.transform.Translate (new Vector2(0,-offset));
+		}
+
+		
+	}
+}
+
+}
