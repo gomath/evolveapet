36db78c45b8b8d6c62a854ff53f029c78603057c
TomLefley <thomas.lefley@gmail.com>
2014-02-20 15:13:02 +0000

    Started to skin the UI
    Added custom buttons and applied these to the main menu

--- evolveapet/eap_frontend/Assets/Scripts/Frontend/MainMenu.cs
+++ evolveapet/eap_frontend/Assets/Scripts/Frontend/MainMenu.cs
@@ -7,7 +7,12 @@ public class MainMenu : MonoBehaviour {
 	float originalHeight = 618.0f;
 	Vector3 scale = new Vector3();
 
+	public GUISkin mySkin;
+
 	void OnGUI () {
+
+		GUI.skin = mySkin;
+
 		scale.x = Screen.width / originalWidth;
 		scale.y = Screen.height / originalHeight;
 		scale.z = 1;
@@ -15,7 +20,9 @@ public class MainMenu : MonoBehaviour {
 
 		// substitute matrix to scale if screen nonstandard
 		GUI.matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, scale);
-		
+
+		GUI.color = Color.blue;
+
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
 		if(GUI.Button(new Rect(700,82,150,40), "New Game")) {
 			//for now loads Animal Scene; should load stable scene
