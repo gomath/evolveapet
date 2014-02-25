e6a183ef0aa3b53e7c7ae86166af18ff07c43b1b
TomLefley <thomas.lefley@gmail.com>
2014-02-25 14:44:49 +0000

    Added a (non-functioning) toolbar

--- evolveapet/eap_frontend/Assets/Scripts/Frontend/Toolbar.cs
+++ evolveapet/eap_frontend/Assets/Scripts/Frontend/Toolbar.cs
@@ -1,22 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Toolbar : MonoBehaviour {
+namespace EvolveAPet{
+	public class Toolbar : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+		float originalWidth = 1098.0f;
+		float originalHeight = 618.0f;
+		Vector3 scale = new Vector3();
 
-	public int toolbarInt = 0;
-	public float barHeight = 0.1f;
-	public string[] toolbarStrings = new string[] {"Toolbar1", "Toolbar2", "Toolbar3"};
-	void OnGUI() {
-		toolbarInt = GUI.Toolbar(new Rect(0, Screen.height-(barHeight*Screen.height), Screen.width, barHeight*Screen.height), toolbarInt, toolbarStrings);
+		public GUISkin mySkin;
+
+		Player currentPlayer;
+
+		public int selected;
+
+		string[] buttons = {"Stable","Animal","Genome","Breed"};
+
+		public Texture coins;
+
+		// Use this for initialization
+		void Start () {
+			currentPlayer = Player.playerInstance; //Checks if there is already an instantiated player
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		void OnGUI() {
+			GUI.skin = mySkin;
+			
+			scale.x = Screen.width / originalWidth;
+			scale.y = Screen.height / originalHeight;
+			scale.z = 1;
+			var svMat = GUI.matrix;
+			// substitute matrix to scale if screen nonstandard
+			GUI.matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, scale);
+
+			GUILayout.BeginArea(new Rect(5,originalHeight-75,originalWidth-5,75));
+			GUILayout.BeginHorizontal();
+
+			GUILayout.Label(coins);
+
+			GUILayout.BeginVertical();
+
+			GUI.color = Color.black;
+			if (currentPlayer != null) {
+				GUILayout.Label(currentPlayer.NickName,  GUILayout.Width (200));
+				//GUILayout.Label (currentPlayer.Points.ToString);
+			} else {
+				GUILayout.Label("Wow, such NickName", GUILayout.Width (200));
+				GUI.skin.label.fontSize = (30);
+				GUILayout.Label ("9000", GUILayout.Width (200));
+				GUI.skin.label.fontSize = (0);
+				//GUILayout.Space(10);
+			}
+
+			GUI.color = Color.white;
+
+			GUILayout.EndVertical();
+
+
+			GUILayout.Toolbar(selected, buttons);
+
+			GUILayout.EndHorizontal();
+			GUILayout.EndArea();
+		}
 	}
 }
