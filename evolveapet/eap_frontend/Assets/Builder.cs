34b483b538cd04c913e1238b39ad8401c2959a5c
NickBlow <nick.blow@live.com>
2014-02-26 21:30:33 +0000

    fixed daily challenge

--- evolveapet/eap_frontend/Assets/Builder.cs
+++ evolveapet/eap_frontend/Assets/Builder.cs
@@ -8,16 +8,16 @@ namespace EvolveAPet {
 	public class Builder : MonoBehaviour {
 		// Use this for initialization
 		Player player = Player.playerInstance;
-
+		
 		void Start () {
 			StartCoroutine("Build");
 		}
 		
 		// Update is called once per frame
 		void Update () {
-		
+			
 		}
-
+		
 		IEnumerator Build() {
 			//Remove the previous animal
 			GameObject.Destroy(GameObject.FindGameObjectWithTag("Animal"));
@@ -33,34 +33,33 @@ namespace EvolveAPet {
 			LinkedList<Animal> list= new LinkedList<Animal> ();
 			list.AddFirst (animal.GetComponent<PhysicalAnimal> ().animal);
 		}
-
+		
 		void OnGUI() {
-			if (GUI.Button (new Rect(Screen.width-200,Screen.height-30,200, 30), "Build")) {
-				StartCoroutine("Build");
-			}
 			string dailyChallengeStr = player.getDailyChallengeString ();
 			string completeBtnStr;
 			string countdown;
-
 			float center = Screen.width * 0.5f -200;
 			GUI.Label (new Rect (center, Screen.height * 0.05f, 400, 50), dailyChallengeStr);
-			if (player.currentDailyChallenge != -1) {//If player already has a daily challenge
-				
+			TimeSpan ts = player.dailyChallengeSetDate.AddDays(1).Subtract(DateTime.Now);
+			countdown = string.Format("{0} Hours, {1} Minutes, and {2} Seconds until new daily challenge!", ts.Hours, ts.Minutes, ts.Seconds);
+			if (player.currentDailyChallenge == -1) {
+				//If player has no current daily challenge, then there are two possibilities. Either he needs a new one, or he needs to wait.
+				if (DateTime.Compare (DateTime.Today, player.dailyChallengeSetDate) != 0){
+					if (GUI.Button (new Rect (center, Screen.height * 0.1f + 20, 400, 30), "New Daily Challenge")) {
+						player.newDailyChallenge(); // If it is not the day of the last completed daily challenge, then make a button to get a new one.
+						//Assumes player cannot travel backwards in time
+					}
+				}else{
+					if (!(ts.Seconds <=0 && ts.Hours<=0 && ts.Minutes<=0)){
+						GUI.Label (new Rect (center, Screen.height * 0.1f, 400, 50), countdown);}
+				}				    
+			}else{
 				if (GUI.Button (new Rect (center, Screen.height * 0.1f +20, 400, 30), "Complete Daily Challenge!")) {
-					player.completeDailyChallenge ();
-								}
-			} else if (player.currentDailyChallenge == -1 && DateTime.Compare (DateTime.Today, player.dailyChallengeSetDate) != 0) {
-				if (GUI.Button (new Rect (center, Screen.height * 0.1f + 20, 400, 30), "New Daily Challenge")) {
-					player.newDailyChallenge(); // If it is not the day of the last completed daily challenge, then make a button to get a new one.
-										//Assumes player cannot travel backwards in time
-								}
-						} else {
-				
-				TimeSpan ts = player.dailyChallengeSetDate.AddDays(1).Subtract(DateTime.Now);//Gets how many hours till 12 AM until day after daily challenge
-				countdown = string.Format("{0} Hours, {1} Minutes, and {2} Seconds until new daily challenge!", ts.Hours, ts.Minutes, ts.Seconds);
-				//Counts down
-				GUI.Label (new Rect (center, Screen.height * 0.1f +20, 410, 30), countdown);
+					player.completeDailyChallenge();
+				}
 			}
+			
+			
 		}
 	}
 }
--- evolveapet/eap_frontend/Assets/Scripts/Backend/Player.cs
+++ evolveapet/eap_frontend/Assets/Scripts/Backend/Player.cs
@@ -7,42 +7,42 @@ using System.IO;
 namespace EvolveAPet
 {
 	[Serializable]
-    public class Player
-    {
-
-        public string NickName { set; get; }
-        public string UserName { set; get; }
-        public int Points { get; set; }
+	public class Player
+	{
+		
+		public string NickName { set; get; }
+		public string UserName { set; get; }
+		public int Points { get; set; }
 		public Stable _stable{get; set;}
 		public int currentDailyChallenge;
 		public DateTime dailyChallengeSetDate;
 		public String[] allDailyChallenges = {"is the biggest","is the smallest","is the reddest","is the greenest", "is the bluest",  
-			 "has spots on as many body parts as possible", "has stripes on as many body parts as possible"}; // add new challenges at end, do not change order
-        public Stable Stable { get { return _stable; } }
+			"has spots on as many body parts as possible", "has stripes on as many body parts as possible"}; // add new challenges at end, do not change order
+		public Stable Stable { get { return _stable; } }
 		public bool[,] guessedGenes; // All the genes in the animal. If the array member at [i][j] is true, that means the the jth gene on the ith chromosome has been guessed
 		public static Player playerInstance = null; // I should have thought of this earlier
 		public DateTime lastSaved;
-
+		
 		public Animal animalForBreeding1;
 		public Animal animalForBreeding2;
 		public Chromosome[] chromosomes1;
 		public Chromosome[] chromosomes2;
 		public int animalToChooseForBreeding;
 		public int remainingAnimalsToBreed;
-
-
+		
+		
 		public Player(Stable s, string username)
-        {
-            Points = 0;
-            UserName = username;
-            NickName = username;
-            _stable = s;
+		{
+			Points = 0;
+			UserName = username;
+			NickName = username;
+			_stable = s;
 			guessedGenes = new bool[7, 6];//This wastes some space, but easy. 
 			newDailyChallenge ();
 			playerInstance = this;
-
-        }
-
+			
+		}
+		
 		public Player(string username)
 		{
 			Points = 0;
@@ -67,10 +67,10 @@ namespace EvolveAPet
 			
 		}
 		public static void autoSave(){//Auto saves the game every 5 minutes
-				if (!(playerInstance.lastSaved.AddMinutes (5) > DateTime.Now)) {
+			if (!(playerInstance.lastSaved.AddMinutes (5) > DateTime.Now)) {
 				playerInstance.saveGame();			
-				}
-		
+			}
+			
 		}
 		public void saveGame(){
 			string path = Environment.CurrentDirectory + "/save.sav";
@@ -80,45 +80,45 @@ namespace EvolveAPet
 			outStream.Close();
 			lastSaved = DateTime.Now;
 		}
-
-        // probably we want to serialize him to to save the game ...
-
-		public void guessGene(int i, int j){
-
-			guessedGenes [i,j] = true;
 		
+		// probably we want to serialize him to to save the game ...
+		
+		public void guessGene(int i, int j){
+			
+			guessedGenes [i,j] = true;
+			
 		}
 		public String getDailyChallengeString(){
 			String str = "Today's challenge is : Breed an animal which ";
-
 			if (currentDailyChallenge == -1){
-				str = "You have already completed a daily challenge today!";
+				str = "You completed your last daily challenge on ";
+				str += dailyChallengeSetDate.ToShortDateString();
 			}
 			else str += allDailyChallenges [currentDailyChallenge] + ".";
 			return str;
-
+			
 		}
 		public void newDailyChallenge(){
 			if(DateTime.Compare(DateTime.Today, dailyChallengeSetDate) == 0) //Daily challenge was set today
 			{
 				currentDailyChallenge = -1;
 			}
-
+			
 			else{		
 				Random r = new Random();
-			currentDailyChallenge = r.Next(0, allDailyChallenges.Length);
-			dailyChallengeSetDate = DateTime.Today;
+				currentDailyChallenge = r.Next(0, allDailyChallenges.Length);
+				dailyChallengeSetDate = DateTime.Today;
 			}
-
+			
 		}
 		public void completeDailyChallenge(){ // the int is the number of points in the 
 			if (currentDailyChallenge != -1) {
-						 // give no points in the event that there is no daily challenge
-			int points = 0 ;
-			int activeAnimalNo = Stable.activeAnimalNumber;
-			Animal activeAnimal = Stable.animalsInStable[activeAnimalNo];
-
-			/*6 traits filled in this order :
+				// give no points in the event that there is no daily challenge
+				int points = 0 ;
+				int activeAnimalNo = Stable.activeAnimalNumber;
+				Animal activeAnimal = Stable.animalsInStable[activeAnimalNo];
+				
+				/*6 traits filled in this order :
 			0.Colour
 			1.Size
 			2.Pattern
@@ -126,33 +126,34 @@ namespace EvolveAPet
 			4.Shape
 			5.Teeth_Shape
 			*/
-
-			for (int n=0; n<Global.NUM_OF_CHROMOSOMES; n++) { // iterate through all the chromosomes
-				if (currentDailyChallenge ==0 || currentDailyChallenge == 1){ // biggest or smallest
-					int size = activeAnimal.Genome.GetTrait(n,1);
-					if ((currentDailyChallenge ==0 && size == 2) || (currentDailyChallenge ==1 && size ==0)) points++;
+				
+				for (int n=0; n<Global.NUM_OF_CHROMOSOMES; n++) { // iterate through all the chromosomes
+					if (currentDailyChallenge ==0 || currentDailyChallenge == 1){ // biggest or smallest
+						int size = activeAnimal.Genome.GetTrait(n,1);
+						if ((currentDailyChallenge ==0 && size == 2) || (currentDailyChallenge ==1 && size ==0)) points++;
+					}
+					if (currentDailyChallenge ==2|| currentDailyChallenge==3||currentDailyChallenge==4){ //reddest, greenest, bluest
+						int colour = activeAnimal.Genome.GetTrait(n,0);
+						// COLOUR = RED << 16 | GREEN << 8 | BLUE
+						int red = (colour & 0x00FF0000) >> 16;
+						int green = (colour & 0x0000FF00)>>8;
+						int blue = (colour & 0x000000FF);
+						if (currentDailyChallenge==2 && red>200) points++;
+						if (currentDailyChallenge==3 && green>200) points++;
+						if (currentDailyChallenge==4&& blue>200) points++;
+					}
+					if (currentDailyChallenge==5 || currentDailyChallenge==6){//most spots or stripes
+						int patternNo = activeAnimal.Genome.GetTrait(n,2);
+						if (currentDailyChallenge==5 && patternNo ==1) points++;
+						if (currentDailyChallenge==6 && patternNo ==2) points++;
+						if (patternNo==3) points++;
+					}
 				}
-				if (currentDailyChallenge ==2|| currentDailyChallenge==3||currentDailyChallenge==4){ //reddest, greenest, bluest
-					int colour = activeAnimal.Genome.GetTrait(n,0);
-					// COLOUR = RED << 16 | GREEN << 8 | BLUE
-					int red = (colour & 0x00FF0000) >> 16;
-					int green = (colour & 0x0000FF00)>>8;
-					int blue = (colour & 0x000000FF);
-					if (currentDailyChallenge==2 && red>200) points++;
-					if (currentDailyChallenge==3 && green>200) points++;
-					if (currentDailyChallenge==4&& blue>200) points++;
-				}
-				if (currentDailyChallenge==5 || currentDailyChallenge==6){//most spots or stripes
-					int patternNo = activeAnimal.Genome.GetTrait(n,2);
-					if (currentDailyChallenge==5 && patternNo ==1) points++;
-					if (currentDailyChallenge==6 && patternNo ==2) points++;
-					if (patternNo==3) points++;
-				}
-			}
-
-			Points = points;
+				
+				Points = points;
+				currentDailyChallenge = -1;
 			}
 		}
-        //Will be adding function for the KnownTraits but not boolean to boolean.
-    }
+		//Will be adding function for the KnownTraits but not boolean to boolean.
+	}
 }
