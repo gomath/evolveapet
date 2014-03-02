c3fea391522f5caa9936a13211d9d428faf3f311
NickBlow <nick.blow@live.com>
2014-03-02 22:26:25 +0000

    Try now

--- evolveapet/eap_frontend/Assets/Scripts/Backend/Stable.cs
+++ evolveapet/eap_frontend/Assets/Scripts/Backend/Stable.cs
@@ -15,7 +15,8 @@ namespace EvolveAPet
 		public bool[] activeStableSlots = {true,true,true,false,false,false};//The stable slots that are unlocked 
 		public bool[] livingAnimals = {false,false,false,false,false,false};//The stable slots which contain a living animal
 		public Animal[] animalsInStable = new Animal[6];
-		public bool guiEnabled = false;
+		[NonSerialized]
+		public bool guiEnabled = true;
 
 		public Animal GetActiveAnimal(){
 			return animalsInStable[activeAnimalNumber];		
